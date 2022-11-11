87e64495329ae78e9b04ebe7adb3e637d9f4ef13
Swag Finger <swagfinger@gmail.com>
2022-11-11 14:36:45 +0800

    finished setting up mongodb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
+
+const mongoUri =
+  'mongodb://swagfinger:HfMcI3h8Enmrg8mA@ac-rqobmv0-shard-00-00.1hy8hpl.mongodb.net:27017,ac-rqobmv0-shard-00-01.1hy8hpl.mongodb.net:27017,ac-rqobmv0-shard-00-02.1hy8hpl.mongodb.net:27017/?ssl=true&replicaSet=atlas-x6rdeg-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+mongoose.connect(mongoUri); //as of mongoose 6
+mongoose.connection.on('connected', () => {
+  console.log('connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+  console.error('error connecting to mongo', err);
+});
 
 //router route
 app.get('/', (req, res) => {
